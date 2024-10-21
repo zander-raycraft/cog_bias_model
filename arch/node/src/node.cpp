42dd1d6408fb502b3ed4749b3530381b067d865f
zander-raycraft <zanderraycraft504@gmail.com>
2024-10-21 15:12:13 -0500

    initial commit

---
+++ arch/node/src/node.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+/**
+ * 
+ * @class: NetworkNode -> base neuron for network
+ *
+ * @note: This node is made to be extendible to various number of inputs and outputs
+ * 
+ */
+
+class NetworkNode {
+    NetworkNode(int num_in);
+
+};
