b3d4c6e18a4f060a20df03628de1f2db99dee231
zander-raycraft <zanderraycraft504@gmail.com>
2024-11-05 16:12:14 -0600

    need to pause

--- arch/layer/headr/layer.h
+++ arch/layer/headr/layer.h
@@ -1,14 +1,16 @@
-#ifndef LAYER_h
+#ifndef LAYER_H
 #define LAYER_H
-
 #include "../../node/headr/node.h"
-
+#include <vector>
 
 /**
  * 
- * 
- * 
+ * @class: NetworkLayer -> base neuron layer for network
+ *
+ * @note: This node is made to be extendible to various number of inputs and outputs
+ *
  */
+
 
 // default ctor
 // copy constructor
@@ -16,8 +18,26 @@
 class NetworkLayer
 {
     public:
-        NetworkLayer(int size); //dfault ctor
-        
+        //Default Constructor
+        NetworkLayer(int size);
+
+        /**
+        *
+        * @breif: copy constructor, deep copy
+        *
+        * @param: copyLayer -> type: NetworkLayer&, layer to copy;
+        *
+        */
+        NetworkLayer(const NetworkLayer& copyLayer) noexcept;
+
+        /**
+        *
+        * @breif: assignment operator
+        *
+        * @param: copyLayer -> type: NetworkLayer&, layer to copy;
+        *
+        */
+        NetworkLayer& operator=(const NetworkLayer& copyLayer) noexcept;
 
     private:
 };
